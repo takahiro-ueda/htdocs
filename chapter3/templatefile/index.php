6dcdfae4c8b50c07af6404de87b53639e353727d
takahiro-ueda <pt25ue@gmail.com>
2020-08-20 16:01:08 +0900

    テンプレートについて

---
+++ chapter3/templatefile/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS ーー>
+<link rel="stylesheet" href="css/style.css">
+
+<title>よくわかるPHPの教科書</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">よくわかるPHPの教科書</h1>
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+/* ここに、PHPのプログラムを記述します　*/
+?>
+</pre>
+</main>
+</body>
+</html>
