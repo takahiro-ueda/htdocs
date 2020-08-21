a2418c0ccdbd4fe994e67526e1bc229cfbd950c4
takahiro-ueda <pt25ue@gmail.com>
2020-08-21 16:16:09 +0900

    タイムスタンプ

--- sample07.php
+++ sample07.php
@@ -1,5 +1,12 @@
 <?php
 //月・日・曜日の英語3文字が表示
-$day = date('n/j(D)');
+// $day = date('n/j(D)');
+// print($day . "\n");
+
+//タイムスタンプ
+$time = time();
+print($time . ("\n"));
+
+$day = date('n/j(D)' . 86400);
 print($day . "\n");
 ?>
