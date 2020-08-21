d65339725326296adb941aa1b35afb5c88a7e27d
takahiro-ueda <pt25ue@gmail.com>
2020-08-21 17:15:42 +0900

    明後日を表示

--- sample07.php
+++ sample07.php
@@ -7,6 +7,14 @@
 $time = time();
 print($time . ("\n"));
 
-$day = date('n/j(D)' . 86400);
-print($day . "\n");
+// $day = date('n/j(D)' . 86400);
+// print($day . "\n");
+
+//strtotimeファンクション
+// $ieyasu = strtotime('1543/1/31', 86400);
+// print($ieyasu);
+
+//明後日の表示
+$day_after_tomorrow = strtotime('+2day');
+print($day_after_tomorrow);
 ?>
