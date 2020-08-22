b5f830e271941bc499bb211d3e96331775929ad4
takahiro-ueda <pt25ue@gmail.com>
2020-08-22 23:52:38 +0900

    練習問題

--- sample08.php
+++ sample08.php
@@ -7,17 +7,21 @@
 // print($week_name);
 
 //インデックス(添え字)
-$week_name = ['日', '月', '火', '水', '木', '金', '土'];
-print($week_name[0]);
-print($week_name[1]);
-print($week_name[2]);
-print($week_name[3]);
-print($week_name[4]);
-print($week_name[5]);
-print($week_name[6]);
+// $week_name = ['日', '月', '火', '水', '木', '金', '土'];
+// print($week_name[0]);
+// print($week_name[1]);
+// print($week_name[2]);
+// print($week_name[3]);
+// print($week_name[4]);
+// print($week_name[5]);
+// print($week_name[6]);
 
-$week = 1 + 3;
-print($week_name[$week] . "\n");
+// $week = 1 + 3;
+// print($week_name[$week] . "\n");
 
-print('今日は' . $week_name[date('w')] . '曜日です！！！！');
+// print('今日は' . $week_name[date('w')] . '曜日です！！！！');
+
+//  練習問題
+$age = ['10代以下', '20代', '30代', '40代', '50代', '60代以上'];
+print($age[0]);
 ?>
