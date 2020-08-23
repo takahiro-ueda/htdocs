16140a696f535ed36d779db55060dba360b79cc9
takahiro-ueda <pt25ue@gmail.com>
2020-08-23 11:24:26 +0900

    練習問題

--- sample09.php
+++ sample09.php
@@ -1,13 +1,29 @@
-<?php
+<!-- <?php 
 // 連想配列
-$fruits =[
-  'apple' => 'りんご',
-  'grape' => 'ぶどう',
-  'lemon' => 'レモン',
-  'tomato' => 'トマト',
-  'peach' => 'もも'
+// $fruits =[
+//   'apple' => 'りんご',
+//   'grape' => 'ぶどう',
+//   'lemon' => 'レモン',
+//   'tomato' => 'トマト',
+//   'peach' => 'もも'
+// ];
+// foreach ($fruits as $english => $japanese) {
+//   print($english . ' : ' . $japanese . "\n");
+// }
+// ?> -->
+
+<select>
+<?php
+//練習問題
+$pratforms = [
+  'win' => 'Windows',
+  'mac' => 'Macintosh',
+  'iphone' => 'iPhone',
+  'ipad' => 'iPod',
+  'android' => 'Android'
 ];
-foreach ($fruits as $english => $japanese) {
-  print($english . ' : ' . $japanese . "\n");
-}
+foreach ($pratforms as $pratformId => $platformValue) :
 ?>
+<option value="<?php echo $platformId; ?>"><?php echo $platformValue; ?></option>
+<?php endforeach; ?>
+</select>
