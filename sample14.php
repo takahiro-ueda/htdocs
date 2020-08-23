dac8ac04b196e9d110780c6efd94d90fc218457f
takahiro-ueda <pt25ue@gmail.com>
2020-08-23 21:24:38 +0900

    ファイルを追記

--- sample14.php
+++ sample14.php
@@ -1,3 +1,7 @@
 <?php
+$doc = file_get_contents('./news_data/news.txt');
+$doc .= "<br />2018-06-02 ニュースを追加しました";
+file_get_contents('./news_data/news.txt', $doc);
+
 readfile('./news_data/news.txt');
 ?>
