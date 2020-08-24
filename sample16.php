6e299401e877cf2c0bd90276bc29572b59bf349c
takahiro-ueda <pt25ue@gmail.com>
2020-08-24 23:03:53 +0900

    グローバル変数

--- sample16.php
+++ sample16.php
@@ -1,9 +1,9 @@
 <?php
 $file = file_get_contents('https://h2o-space.com/feed/json');
-$json = json_decode($file);　//「デコード(Decode)」というは「コードをほぐす」といった意味
+$json = json_decode($file); //「デコード(Decode)」というは「コードをほぐす」といった意味
 
 // foreach構文
-foreach($json->items as $item) :　//XMLと違って「channel」という親要素はない
+foreach($json->items as $item) ://XMLと違って「channel」という親要素はない
 ?>
 ・<a href="<?php print($item->url); ?>"><?php print($item->title); ?></a>
 <?php
---
+++ sample17/submit.php
@@ -0,0 +1,3 @@
+<?php
+print($_REQUEST['my_name']);
+?>
