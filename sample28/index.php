972d261c02c4e75dcfc08a5334f619054a1c93d0
takahiro-ueda <pt25ue@gmail.com>
2020-08-26 11:26:02 +0900

    コード化

--- sample28/index.php
+++ sample28/index.php
@@ -1,3 +1,6 @@
+<!-- 「enctype」＝「encode type」の略でコード化（エンコード）と呼ばれる作業をします
+「multipart/form-data」はこれまでの文字情報のみのフォームに加えて、ファイルをそのまま送信することができる方式
+※ファイル送信はgetではなく必ずpost -->
 <form action="submit.php" method="post" enctype="multipart/form-data">
 
 <input type="text" name="ok">
