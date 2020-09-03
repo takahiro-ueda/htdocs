41d586828fc0c2618588677392acce728aa8ef48
takahiro-ueda <pt25ue@gmail.com>
2020-09-03 11:39:56 +0900

    より安全にURLパラメーターを受け取るには

--- memo/memo.php
+++ memo/memo.php
@@ -6,6 +6,12 @@ try {
 } catch (PDOException $e) {
   echo 'DB接続エラー：　' . $e->getMessage();
 }
+// より安全にURLパラメーターを受け取るには
+$id = $_REQUEST['id'];
+if (!is_numeric($id) || $id <= 0) { //「is_numeric」は「数字かどうか」を判定するファンクション[数字が指定有はtrue,無はfalse]
+  print('1以上の数字で指定してください');
+  exit();
+}
 
 //すぐに実行されず次の「execute」メソッドで実行される, IDはURLパラメーターで指定されるため「$_REQUEST」または「$_GET」を使って取得
 $memos = $db->prepare('SELECT * FROM memos WHERE id=?');
