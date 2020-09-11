1a1c8ef6a5b36d7a28758f70d5776084b9f14bc4
takahiro-ueda <pt25ue@gmail.com>
2020-09-11 22:19:01 +0900

    DBに登録

--- post/join/check.php
+++ post/join/check.php
@@ -1,12 +1,30 @@
 <?php
 session_start();
+require('../dbconnect.php'); //dbconnect.phpを呼び出している
 
 if (!isset($_SESSION['join'])) { //入力したセッションに記録しておき、「$_SESSION['join']」に何も含まれていない場合はcheck.php。
   header('Location: index.php');
+  exit();
+}
+
+if (!empty($_POST)) { //prepareメソッドでSQLを組み立て、セッションに保存した値をそれぞれセット。この時、パスワードのみ「shal」ファンクションを使って暗号化
+  //登録処理をする
+  $statement = $db->prepare('INSERT INTO members SET name=?, email=?, password=?, picture=?, created=NOW()');
+  echo $ret = $statement->execute(array(
+    $_SESSION['join']['name'],
+    $_SESSION['join']['email'],
+    shal($SESSION['join']['password']),
+    $_SESSION['join']['image']
+  ));
+  unset($_SESSION['join']); //「unset」ファンクションを使って、「$_SESSION['join']」変数、つまり入力情報を削除。DBには既に登録したので重複登録などを防ぐために、セッションから消す
+
+  header('Location: thanks.php');
+  exit();
 }
 ?>
 ・・・
 <form action="" method="post">
+  <input type="hidden" name="action" value="submit" />
   <dl>
     <dt>ニックネーム</dt>
     <dd>
