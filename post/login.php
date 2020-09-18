7b4c837f9c6fd0f14aae585efa211d4c3f401190
takahiro-ueda <pt25ue@gmail.com>
2020-09-18 15:56:10 +0900

    ログイン画面のHTMLを作る

---
+++ post/login.php
@@ -0,0 +1,22 @@
+<div id="lead">
+  <p>メールアドレスとパスワードを記入してログインしてください。</p>
+  <p>入会手続きがまだの方はこちらからどうぞ。</p>
+  <p>&raquo;<a href="join/">入会手続きをする</a></p>
+</div>
+<form action="" method="post">
+  <dl>
+    <dt>メールアドレス</dt>
+    <dd>
+      <input type="text" name="email" size="35" maxlength="255" />
+    </dd>
+    <dt>パスワード</dt>
+    <dd>
+      <input type="password" name="password" size="35" maxlength="255" />
+    </dd>
+    <dt>ログイン情報の記録</dt>
+    <dd>
+      <input id="save" type="checkbox" name="save" value="on"><label for="save">次回からは自動的にログインする</label>
+    </dd>
+  </dl>
+  <div><input type="submit" value="ログインする" /></div>
+</form>
