63d239d0dc4321d4491ffcd1a7b11b592a654b95
takahiro-ueda <pt25ue@gmail.com>
2020-09-20 12:19:05 +0900

    投稿画面のHTMLを作成

---
+++ post/index.php
@@ -0,0 +1,11 @@
+<form action="" method="post">
+  <dl>
+    <dt>メッセージをどうぞ</dt>
+    <dd>
+      <textarea name="message" cols="50" rows="5"></textarea>
+    </dd>
+  </dl>
+  <div>
+    <input type="submit" value="投稿する" />
+  </div>
+</form>
