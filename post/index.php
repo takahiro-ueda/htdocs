ddecc51d522941de1d316695b33f14918fe8f982
takahiro-ueda <pt25ue@gmail.com>
2020-09-20 12:38:43 +0900

    ログイン状態をチェック

--- post/index.php
+++ post/index.php
@@ -1,6 +1,38 @@
+<?php
+session_start();
+require('dbconnect.php');
+
+if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) { //ログインしている状態であるかを検査（idがセッションに記録されている、最後の行動から1時間以内）
+  //ログインしている
+  $_SESSION['time'] = time();
+
+  $members = $db->prerare('SELECT * FROM members WHERE id=?');
+  $members->execute(array($_SESSION['id']));
+  $member = $members->fetch();
+} else {
+  //ログインしていない
+  header('Location: login.php'); exit();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" type="text/css" />
+
+<title>会員登録</title>
+</head>
+<body>
+<header>
+<h1>会員登録</h1>
+</header>
+
+<main>
 <form action="" method="post">
   <dl>
-    <dt>メッセージをどうぞ</dt>
+    <dt><?php echo htmlspecialchars($member['name'], ENT_QUOTES); ?>さん、メッセージをどうぞ</dt>
     <dd>
       <textarea name="message" cols="50" rows="5"></textarea>
     </dd>
@@ -9,3 +41,6 @@
     <input type="submit" value="投稿する" />
   </div>
 </form>
+</main>
+</body>
+</html>
