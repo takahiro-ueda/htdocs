3911ecd38ff0ce5fbb543da25db162c90703b7ca
takahiro-ueda <pt25ue@gmail.com>
2020-09-21 08:32:32 +0900

    投稿内容を表示部分の修正

--- post/index.php
+++ post/index.php
@@ -58,13 +58,13 @@ $posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE
   </div>
 </form>
 <?php
-foreach ($posts as $post);
+foreach ($posts as $post):
 ?>
-<div class="msg">
-  <img src="member_picture/<?php echo htmlspecialchars($post['picture'],ENT_QUOTES); ?>" width="48" height="48" alt="<?php echo htmlspecialchars($post['name'], ENT_QUOTES; ?>" />
-  <p><?php echo htmlspecialchars($post['message'], ENT_QUOTES); ?><span class="name">（<?php echo htmlspecialchars($post['name'], ENT_QUOTES）; ?></span></p>
-  <p class="day"><?php echo htmlspecialchars($post['created'], ENT_QUOTES); ?></p>
-</div>
+  <div class="msg">
+    <img src="member_picture/<?php echo htmlspecialchars($post['picture'], ENT_QUOTES); ?>" width="48" height="48" alt="<?php echo htmlspecialchars($post['name'], ENT_QUOTES); ?>" />
+    <p><?php echo htmlspecialchars($post['message'], ENT_QUOTES); ?><span class="name">（<?php echo htmlspecialchars($post['name'], ENT_QUOTES); ?>）</span></p>
+    <p class="day"><?php echo htmlspecialchars($post['created'], ENT_QUOTES); ?></p>
+  </div>
 <?php
 endforeach;
 ?>
