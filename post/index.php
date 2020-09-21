f8110315cf673e99b0cfbeae44ca42083a0e14d2
takahiro-ueda <pt25ue@gmail.com>
2020-09-21 11:48:50 +0900

    URLをリンクにするファンクション

--- post/index.php
+++ post/index.php
@@ -43,6 +43,12 @@ if (isset($_REQUEST['res'])) {
 function h($value) { //
   return htmlspecialchars($value, ENT_QUOTES);
 }
+
+//本文内のURLにリンクを設置
+function makeLink($value) {
+  return mb_ereg_replace("(https?)(://[[:alnum:]\+\$\;\?\.%,!#~*/:@&=_-]+)", '<a href="\1\2">\1\2</a>' , $value);
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
